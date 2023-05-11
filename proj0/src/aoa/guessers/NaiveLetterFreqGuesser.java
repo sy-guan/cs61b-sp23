3b492f192e92634cc42cef1863a3e34c89e15167
sy-guan <shuyi.guan@uzh.ch>
2023-05-11 14:10:50 +0200

    Naive Guesser updated

--- proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
@@ -1,8 +1,11 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class NaiveLetterFreqGuesser implements Guesser {
     private final List<String> words;
@@ -20,15 +23,51 @@ public class NaiveLetterFreqGuesser implements Guesser {
     /** Returns a map from a given letter to its frequency across all words.
      *  This task is similar to something you did in hw0b! */
     public Map<Character, Integer> getFrequencyMap() {
-        // TODO: Fill in this method.
-        return null;
+        // Finished: Fill in this method.
+        Map<Character, Integer> m = new HashMap<>();
+        for (String w : words){
+            for (int i = 0; i < w.length(); i++){
+                char c = w.charAt(i);
+                if (!m.containsKey(c)){
+                    m.put(c,1);
+                }else{
+                    m.put(c,m.get(c)+1);
+                }
+            }
+        }
+        return m;
     }
 
     /** Returns the most common letter in WORDS that has not yet been guessed
      *  (and therefore isn't present in GUESSES). */
     public char getGuess(List<Character> guesses) {
         // TODO: Fill in this method.
-        return '?';
+        // Tree map stores keys in sorted order
+        Map<Character, Integer> m = new TreeMap<>();
+        Map<Character, Integer> freq = getFrequencyMap();
+        if (freq.isEmpty()){
+            return '?';
+        }
+        // Get largest frequency.
+        int largest = 0;
+        for (char w : freq.keySet()){
+            int f = freq.get(w);
+            if (!guesses.contains(w)){
+                m.put(w,f);
+                if (f>= largest) {
+                    largest = f;
+                }
+            }
+        }
+        char letter = '\0';
+        for (char w: m.keySet()){
+            if (m.get(w) == largest){
+                letter = w;
+                break;
+            }
+        }
+        return letter;
+
     }
 
     public static void main(String[] args) {
